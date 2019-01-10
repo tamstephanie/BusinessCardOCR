74b17a0bf3a08229fb50d055dcc0dfb17f9ba077
Stephanie Tam <tam.stephaniek@gmail.com>
2019-01-09 23:17:12 -0500

    figured out how to parse for the name

--- businessCardParser.py
+++ businessCardParser.py
@@ -11,20 +11,21 @@ class BusinessCardParser:
         with open(document, 'r') as card:
             info = [line.strip() for line in card]
         
-        # Check lines for the necessary info
+        # Check business card for the necessary info
         for line in info:
-            # TODO: figure out how to get name
-            if():
-                name = "test"
-            
-            # Check if phone number format, and strip to just numbers
+            # Search card for person's name
+            if not any(value in line for value in ("Software", "Engineer", "Developer", "Technologies", "Ltd")):
+                if(re.search("^[A-Z][a-z]+\s[A-Z][a-z]+", line)):
+                    name = line
+                    
+            # Search for phone number & just get the numbers
             if(re.search("^[(]|^\d|^Phone:|^Tel:", line)):
                 # Remove unecessary characters and/or strings from phone number
                 for c in chars:
                     line = line.replace(c, '')
                 phone = re.sub('Phone|Tel', '', line)
-
-            # Check to see if email
+            
+            # Search card for email address
             if "@" in line:
                 email = line
             
